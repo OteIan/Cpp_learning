5a948877737cb4b4bd584426ce4cbe34b76631b3
Ote Ian <oteian9@gmail.com>
2023-10-26 09:01:14 +0300

    .

--- Exercise_1/10-sales.cpp
+++ Exercise_1/10-sales.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     // 15 hours, 30 mins and 30 sec => 55,830 seconds
     int seconds = 55830.0;
-    cout << (3600.0 / seconds) * 6.0;
+    cout << (3600.0 / seconds) * 6.0 << endl;
 
     return 0;
 }
