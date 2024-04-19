3d15246e64e39dd602b39a95bb4d2301b6d7f32e
OteIan <oteian9@gmail.com>
2024-04-19 20:21:48 +0300

    Fix: Comments

--- DSA/Searching_Algorithms/Assignments/SortedStack_with_BinarySearch.cpp
+++ DSA/Searching_Algorithms/Assignments/SortedStack_with_BinarySearch.cpp
@@ -167,77 +167,77 @@ public:
     }
 };
 
-// int main() {
-//     Stack s1;
-//     int option;
-//     string RegNo, name;
+int main() {
+    Stack s1;
+    int option;
+    string RegNo, name;
 
-//     do {
-//         cout << "What operation do you want to perform?" << "Select Option number. \nEnter 0 to exit." << endl;
-//         cout << "1. Push()" << endl;
-//         cout << "2. Pop()" << endl;
-//         cout << "3. isEmpty()" << endl;
-//         cout << "4. peek()" << endl;
-//         cout << "5. count()" << endl;
-//         cout << "6. display()" << endl;
-//         cout << "7. Clear Screen" << endl << endl;
-//         cin >> option;
-//         // Node n1 = new Node();
-//         Node *new_node = new Node();
+    do {
+        cout << "What operation do you want to perform?" << "Select Option number. \nEnter 0 to exit." << endl;
+        cout << "1. Push()" << endl;
+        cout << "2. Pop()" << endl;
+        cout << "3. isEmpty()" << endl;
+        cout << "4. peek()" << endl;
+        cout << "5. count()" << endl;
+        cout << "6. display()" << endl;
+        cout << "7. Clear Screen" << endl << endl;
+        cin >> option;
+        // Node n1 = new Node();
+        Node *new_node = new Node();
 
-//         switch (option) {
-//         case 0:
-//             break;
-//         case 1:
-//             cout << "Enter RegNo of student: ";
-//             cin >> RegNo;
-//             cout << "Enter Name of student: ";
-//             cin >> name;
-//             new_node->RegNo = RegNo;
-//             new_node->name = name;
-//             s1.push(new_node);
-//             break;
-//         case 2:
-//             cout << "Pop Function Called - Poped Value: " << endl;
-//             new_node = s1.pop();
-//             if (new_node != NULL) {
-//                 cout << "TOP of Stack is: (" << new_node->RegNo << "," << new_node->name << ")";
-//                 delete new_node;
-//             }
-//             cout << endl;
-//             break;
-//         case 3:
-//             if (s1.isEmpty())
-//                 cout << "Stack is Empty" << endl << endl;
-//             else
-//                 cout << "Stack is not Empty" << endl << endl;
-//             break;
-//         case 4:
-//             if (s1.isEmpty()) {
-//                 cout << "Stack is Empty" << endl;
-//             }
-//             else {
-//                 cout << "PEEK Function Called : " << endl;
-//                 new_node = s1.peek();
-//                 cout << "TOP of Stack is: (" << new_node->RegNo << "," << new_node->name << ")" << endl << endl;
-//             }
-//             break;
-//         case 5:
-//             cout << "Count Function Called: " << endl;
-//             cout << "No of nodes in the Stack: " << s1.count() << endl << endl;
-//             break;
-//         case 6:
-//             cout << "Display Function Called - " << endl;
-//             s1.display();
-//             cout << endl;
-//             break;
-//         case 7:
-//             system("clear");
-//             break;
-//         default:
-//             cout << "Enter Proper Option number " << endl << endl;
-//         }
-//     } while (option != 0);
+        switch (option) {
+        case 0:
+            break;
+        case 1:
+            cout << "Enter RegNo of student: ";
+            cin >> RegNo;
+            cout << "Enter Name of student: ";
+            cin >> name;
+            new_node->RegNo = RegNo;
+            new_node->name = name;
+            s1.push(new_node);
+            break;
+        case 2:
+            cout << "Pop Function Called - Poped Value: " << endl;
+            new_node = s1.pop();
+            if (new_node != NULL) {
+                cout << "TOP of Stack is: (" << new_node->RegNo << "," << new_node->name << ")";
+                delete new_node;
+            }
+            cout << endl;
+            break;
+        case 3:
+            if (s1.isEmpty())
+                cout << "Stack is Empty" << endl << endl;
+            else
+                cout << "Stack is not Empty" << endl << endl;
+            break;
+        case 4:
+            if (s1.isEmpty()) {
+                cout << "Stack is Empty" << endl;
+            }
+            else {
+                cout << "PEEK Function Called : " << endl;
+                new_node = s1.peek();
+                cout << "TOP of Stack is: (" << new_node->RegNo << "," << new_node->name << ")" << endl << endl;
+            }
+            break;
+        case 5:
+            cout << "Count Function Called: " << endl;
+            cout << "No of nodes in the Stack: " << s1.count() << endl << endl;
+            break;
+        case 6:
+            cout << "Display Function Called - " << endl;
+            s1.display();
+            cout << endl;
+            break;
+        case 7:
+            system("clear");
+            break;
+        default:
+            cout << "Enter Proper Option number " << endl << endl;
+        }
+    } while (option != 0);
 
-//     return 0;
-// }
+    return 0;
+}
