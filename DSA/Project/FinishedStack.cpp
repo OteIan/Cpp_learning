551f0efd0b1916f2b4e06ae417da92e23c7b8803
OteIan <oteian9@gmail.com>
2024-04-21 02:23:45 +0300

    Added spaces to display Moved the main function to another file

--- DSA/Project/FinishedStack.cpp
+++ DSA/Project/FinishedStack.cpp
@@ -84,10 +84,11 @@ public:
 };
 
 string print_node(Node *temp) {
-    return "[" + temp->RegNo + "," + temp->name + "," + to_string(temp->age) + "," + temp->gender + "," +  "Grade: " + temp->grade + "," + to_string(temp->aggregate) + "/1100" + ", Grades => (" + to_string(temp->AppliedMaths1A) + "," + to_string(temp->AppliedMaths1B) + "," + to_string(temp->ComputerScience) + "," + to_string(temp->DSA) + "," + to_string(temp->ECT) + "," + to_string(temp->Physics1A) + "," + to_string(temp->Physics1B) + "," + to_string(temp->PureMaths1A) + "," + to_string(temp->PureMaths1B) + "," + to_string(temp->WorkshopTechnology1) + "," + to_string(temp->WorkshopTechnology2) + ")" + "]";
+    return "[" + temp->RegNo + ", " + temp->name + ", " + to_string(temp->age) + ", " + temp->gender + ", " +  "Grade: " + temp->grade + ", " + to_string(temp->aggregate) + "/1100" + ", Grades => (" + to_string(temp->AppliedMaths1A) + ", " + to_string(temp->AppliedMaths1B) + ", " + to_string(temp->ComputerScience) + ", " + to_string(temp->DSA) + ", " + to_string(temp->ECT) + ", " + to_string(temp->Physics1A) + ", " + to_string(temp->Physics1B) + ", " + to_string(temp->PureMaths1A) + ", " + to_string(temp->PureMaths1B) + ", " + to_string(temp->WorkshopTechnology1) + ", " + to_string(temp->WorkshopTechnology2) + ")" + "]";
 }
 
 class Stack {
+// TO-DO: Order BY: Age, Grade, Aggregate, Each Unit Grade
 public:
     Node *top;
 
@@ -180,7 +181,7 @@ public:
     }
 
     void display() {
-        cout << "----------\nFormat of each student ==> [Reg No., Name, Age, Gender, Mean Grade, Aggregate marks, Grades => (Applied Math 1A, Appplied Math 1B, Computer Science, DSA, ECT, Physics 1A, Physics 1B, Pure Maths 1A, Pure Maths 1B, Workshop Technology I, Workshop Technology II)]\n----------" << endl;
+        cout << "----------\nFormat for grades ==> (Applied Math 1A, Appplied Math 1B, Computer Science, DSA, ECT, Physics 1A, Physics 1B, Pure Maths 1A, Pure Maths 1B, Workshop Technology I, Workshop Technology II)\n----------" << endl;
         cout << "All values in the Stack are :" << endl;
         Node *temp = top;
         while (temp != NULL) {
@@ -197,113 +198,3 @@ public:
     }
 
 };
-
-int main() {
-    Stack s1;
-    int option, sum = 0;
-    float mean_grade = 0;
-
-    do {
-        cout << "What operation do you want to perform?" << "Select Option number. \nEnter 0 to exit." << endl;
-        cout << "1. Push()" << endl;
-        cout << "2. Pop()" << endl;
-        cout << "3. isEmpty()" << endl;
-        cout << "4. peek()" << endl;
-        cout << "5. count()" << endl;
-        cout << "6. display()" << endl;
-        cout << "7. Clear Screen" << endl << endl;
-        cin >> option;
-        // Node n1 = new Node();
-        Node *new_node = new Node();
-
-        switch (option) {
-        case 0:
-            break;
-        case 1:
-            cout << "Enter RegNo of student: ";
-            cin >> new_node->RegNo;
-            cout << "Enter Name of student: ";
-            cin >> new_node->name;
-            cout << "Enter age of student: ";
-            cin >> new_node->age;
-            cout << "Enter Gender of student: ";
-            cin >> new_node->gender;
-
-            cout << "------STUDENT'S GRADES------\nEnter AppliedMaths1A marks of student: ";
-            cin >> new_node->AppliedMaths1A;
-            cout << "Enter AppliedMaths1B marks of student: ";
-            cin >> new_node->AppliedMaths1B;
-            cout << "Enter ComputerScience marks of student: ";
-            cin >> new_node->ComputerScience;
-            cout << "Enter DSA marks of student: ";
-            cin >> new_node->DSA;
-            cout << "Enter ECT marks of student: ";
-            cin >> new_node->ECT;
-            cout << "Enter Physics1A marks of student: ";
-            cin >> new_node->Physics1A;
-            cout << "Enter Physics1B marks of student: ";
-            cin >> new_node->Physics1B;
-            cout << "Enter PureMaths1A marks of student: ";
-            cin >> new_node->PureMaths1A;
-            cout << "Enter PureMaths1B marks of student: ";
-            cin >> new_node->PureMaths1B;
-            cout << "Enter WorkshopTechnology1 marks of student: ";
-            cin >> new_node->WorkshopTechnology1;
-            cout << "Enter WorkshopTechnology2 marks of student: ";
-            cin >> new_node->WorkshopTechnology2;
-
-            new_node->aggregate = (new_node->AppliedMaths1A + new_node->AppliedMaths1B + new_node->ComputerScience + new_node->DSA + new_node->ECT + new_node->Physics1A + new_node->Physics1B + new_node->PureMaths1A + new_node->PureMaths1B + new_node->WorkshopTechnology1 + new_node->WorkshopTechnology2);
-            mean_grade = new_node->aggregate / 11;
-            new_node->grade = grade_calculator(mean_grade);
-            
-            s1.push(new_node);
-            break;
-        case 2:
-            cout << "Pop Function Called - Poped Value: " << endl;
-            new_node = s1.pop();
-            if (new_node != NULL) {
-                cout << "TOP of Stack is: " << print_node(new_node) << endl;
-                delete new_node;
-            }
-            cout << endl;
-            break;
-        case 3:
-            if (s1.isEmpty())
-                cout << "Stack is Empty" << endl << endl;
-            else
-                cout << "Stack is not Empty" << endl << endl;
-            break;
-        case 4:
-            if (s1.isEmpty()) {
-                cout << "Stack is Empty" << endl;
-            }
-            else {
-                cout << "PEEK Function Called : " << endl;
-                new_node = s1.peek();
-                cout << "TOP of Stack is: " << print_node(new_node) << endl << endl;
-            }
-            break;
-        case 5:
-            cout << "Count Function Called: " << endl;
-            cout << "No of nodes in the Stack: " << s1.count() << endl << endl;
-            break;
-        case 6:
-            cout << "Display Function Called - " << endl;
-            s1.display();
-            cout << endl;
-            break;
-        case 7:
-            system("clear");
-            break;
-        default:
-            cout << "Enter Proper Option number " << endl << endl;
-        }
-    } while (option != 0);
-
-    while (!s1.isEmpty()) {
-        delete s1.pop();
-    }
-    cout << "Deleted Entire Stack" << endl;
-
-    return 0;
-}
